00d0bd7d3296fbec5562b28b78fbc58fabb88890
DaisukeMiyazaki <aubozumom@icloud.com>
2023-09-13 09:53:36 +0900

    feat: changed port to 10000

--- up-down.sh
+++ up-down.sh
@@ -5,7 +5,7 @@ if [ -z "$DCPS" ]; then
     echo "Launch..."
     docker-compose build
     # xdg-open http://localhost:8888
-    open http://localhost:8888
+    open http://localhost:10000
     docker-compose up
 else
     echo "Shutdown..."
